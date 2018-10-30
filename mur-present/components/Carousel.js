b39ec322f90a343ce7f42b89d86ca88e83859d87
Ryan Garcia <garciaryan007@yahoo.com>
2018-10-30 15:30:20 -0700

    sliders work, images do not

--- mur-present/components/Carousel.js
+++ mur-present/components/Carousel.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Dimensions, View, Text, StyleSheet, Image } from 'react-native';
+import { 
+  Dimensions, 
+  View, 
+  Text, 
+  StyleSheet, 
+  Image, 
+  Platform, 
+  TouchableOpacity 
+} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+function wp (percentage) {
+  const value = (percentage * viewportWidth) / 100;
+  return Math.round(value);
+}
+
+
+const slideHeight = viewportHeight * 0.36;
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+const entryBorderRadius = 8;
+const IS_IOS = Platform.OS === 'ios';
+const even = true;
+
+
 export const sliderWidth = viewportWidth;
-export const sliderHeight = viewportHeight / 2;
-export const itemWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * 2;
+
 export default class MyCarousel extends React.Component {
 
   constructor() {
@@ -27,11 +50,26 @@ export default class MyCarousel extends React.Component {
   }
   _renderItem ({item, index}) {
       return (
-          <View style={styles.slide}>
-            <Image source={item.pic} />
-            <Text style={styles.title}>{ item.title }</Text>
-            <Text style={styles.subtitle}>{ item.subtitle }</Text>
+        <TouchableOpacity
+        activeOpacity={1}
+        style={styles.slideInnerContainer}
+        onPress={() => { alert(`You've clicked '${item.title}'`); }}
+        >
+          <View style={styles.shadow} />
+          <View style={[styles.imageContainer, even ? styles.imageContainerEven : {}]}>
+              { item.pic }
+              <View style={[styles.radiusMask, even ? styles.radiusMaskEven : {}]} />
           </View>
+          <View style={[styles.textContainer, even ? styles.textContainerEven : {}]}>
+              { item.title }
+              <Text
+                style={[styles.subtitle, even ? styles.subtitleEven : {}]}
+                numberOfLines={2}
+              >
+                  { item.subtitle }
+              </Text>
+          </View>
+        </TouchableOpacity>
       );
   }
 
@@ -42,7 +80,6 @@ export default class MyCarousel extends React.Component {
             data={this.state.images}
             renderItem={this._renderItem}
             sliderWidth={sliderWidth}
-            sliderHeight={sliderHeight}
             itemWidth={itemWidth}
           />
       );
@@ -50,10 +87,81 @@ export default class MyCarousel extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  slide: {
-
+  slideInnerContainer: {
+    width: itemWidth,
+    height: slideHeight,
+    paddingHorizontal: itemHorizontalMargin,
+    paddingBottom: 18 // needed for shadow
+  },
+  shadow: {
+      position: 'absolute',
+      top: 0,
+      left: itemHorizontalMargin,
+      right: itemHorizontalMargin,
+      bottom: 18,
+      shadowColor: '#000',
+      shadowOpacity: 0.25,
+      shadowOffset: { width: 0, height: 10 },
+      shadowRadius: 10,
+      borderRadius: entryBorderRadius
+  },
+  imageContainer: {
+      flex: 1,
+      marginBottom: IS_IOS ? 0 : -1, // Prevent a random Android rendering issue
+      backgroundColor: '#fff',
+      borderTopLeftRadius: entryBorderRadius,
+      borderTopRightRadius: entryBorderRadius
+  },
+  imageContainerEven: {
+      backgroundColor: '#000'
+  },
+  image: {
+      ...StyleSheet.absoluteFillObject,
+      resizeMode: 'cover',
+      borderRadius: IS_IOS ? entryBorderRadius : 0,
+      borderTopLeftRadius: entryBorderRadius,
+      borderTopRightRadius: entryBorderRadius
+  },
+  // image's border radius is buggy on iOS; let's hack it!
+  radiusMask: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: entryBorderRadius,
+      backgroundColor: 'white'
+  },
+  radiusMaskEven: {
+      backgroundColor: '#000'
+  },
+  textContainer: {
+      justifyContent: 'center',
+      paddingTop: 20 - entryBorderRadius,
+      paddingBottom: 20,
+      paddingHorizontal: 16,
+      backgroundColor: 'white',
+      borderBottomLeftRadius: entryBorderRadius,
+      borderBottomRightRadius: entryBorderRadius
+  },
+  textContainerEven: {
+      backgroundColor: '#000'
   },
   title: {
-
+      color: '#000',
+      fontSize: 13,
+      fontWeight: 'bold',
+      letterSpacing: 0.5
+  },
+  titleEven: {
+      color: 'white'
+  },
+  subtitle: {
+      marginTop: 6,
+      color: '#cea9a9',
+      fontSize: 12,
+      fontStyle: 'italic'
+  },
+  subtitleEven: {
+      color: 'rgba(255, 255, 255, 0.7)'
   }
 })
